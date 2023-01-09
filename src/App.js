0075befa1bd4ef5de6edd423d61fccea3445b1f5
Ageng Wibowo <ageng.wibowo@dataon.com>
2023-01-09 16:09:55 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import OneSignal from "react-onesignal";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        runOneSignal();
+    }, []);
+
+    async function runOneSignal() {
+        await OneSignal.init({
+            appId: "9f0514ae-7e81-4ae0-9480-0208b8dff39e",
+            allowLocalhostAsSecureOrigin: true,
+        });
+        OneSignal.showSlidedownPrompt();
+    }
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
